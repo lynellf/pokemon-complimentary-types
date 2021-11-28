8f300be74735bc1aae9356e35f9e56df5a4f94b0
Ezell <lynellf@gmail.com>
2021-11-28 15:20:02 -0600

    add ability to visualize types as a graph

---
+++ Node.ts
@@ -0,0 +1,35 @@
+import { Types } from "./types";
+
+type TNode = Map<
+  "strengths" | "weaknesses" | "resistances" | "resistedBy",
+  Types[]
+>;
+
+interface INodeDeps {
+  getStrengths: (type: Types) => Types[];
+  getWeaknesses: (type: Types) => Types[];
+  getResistances: (type: Types) => Types[];
+  getResistedBy: (type: Types) => Types[];
+}
+
+interface INode {
+  create: (type: Types) => TNode;
+}
+
+const node =
+  (deps: INodeDeps) =>
+  (type: Types): TNode =>
+    new Map([
+      ["resistedBy", deps.getResistedBy(type)],
+      ["strengths", deps.getStrengths(type)],
+      ["weaknesses", deps.getWeaknesses(type)],
+      ["resistances", deps.getResistances(type)],
+    ]);
+
+export default (deps: INodeDeps): INode => {
+  const create = node(deps);
+
+  return {
+    create,
+  };
+};
---
+++ __tests__/Node.test.ts
@@ -0,0 +1,38 @@
+import Node from "../Node";
+import { baseTypes } from "../types";
+import Resistances from "../Resistances";
+import Weaknesses from "../Weaknesses";
+import Strengths from "../Strengths";
+import Offense from "../Offense";
+import Defense from "../Defense";
+
+const { getWeaknesses, getResistances } = Defense({
+  baseTypes,
+  Weaknesses,
+  Resistances,
+});
+const { getStrengths, getResistedBy } = Offense({
+  typeChart: baseTypes.defenseChart,
+  Strengths,
+});
+const { create } = Node({
+  getStrengths,
+  getWeaknesses,
+  getResistances,
+  getResistedBy,
+});
+
+describe("type node creation", () => {
+  it("should create a node", () => {
+    const node = create("fairy");
+    const strengths = node.get("strengths");
+    const weaknesses = node.get("weaknesses");
+    const resistances = node.get("resistances");
+    const resistedBy = node.get("resistedBy");
+
+    expect(strengths.length).toBeGreaterThan(0);
+    expect(weaknesses.length).toBeGreaterThan(0);
+    expect(resistances.length).toBeGreaterThan(0);
+    expect(resistedBy.length).toBeGreaterThan(0);
+  });
+});
