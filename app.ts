6eeae76fcc2a004715ca9711e1a0f3141d7cefc4
Ezell <lynellf@gmail.com>
2021-11-30 06:52:58 -0600

    update main entrypoint

--- app.ts
+++ app.ts
@@ -7,6 +7,9 @@ import Node from "./Node";
 import Traversal from "./Traversal";
 import Strengths from "./Strengths";
 import UserInput from "./UserInput";
+import DualTypeNode from "./DualTypeNode";
+import DualTypeTraversal from "./DualTypeTraversal";
+import DualTypeUserInput from "./DualTypeUserInput";
 import { baseTypes } from "./types";
 
 const getIdealTeam = TeamBuilder({
@@ -19,32 +22,59 @@ const getIdealTeam = TeamBuilder({
   Traversal,
   Strengths,
   UserInput,
+  DualTypeNode,
+  DualTypeTraversal,
+  DualTypeUserInput,
 });
 
 function main() {
   const query: any = [
-    // "fire",
-    // "fighting",
-    "water",
-    "flying",
-    // "poison",
-    // "dark",
-    // "grass",
-    // "ice",
-    "psychic",
-    "steel",
-    // "dragon",
-    // "steel",
+    // ["fire", "fighting"],
+    // ["water", "flying"],
+    // ["poison", "dark"],
+    // ["psychic"],
+    // ["ground", "flying"],
+    // ["grass", "ice"],
+    // ["psychic", "steel"],
+    // ["dragon", "steel"],
+    ["psychic"],
+    // ["water"],
+    // ["psychic", "steel"],
+    // ["dragon", "steel"],
+    // ["ground", "dragon"],
+    // ["fairy", "flying"],
+    // ["grass", "poison"],
   ];
+  // const query: any = [
+  //   "water",
+  //   "flying",
+  //   "fire",
+  //   "fighting",
+  //   "poison",
+  //   "dark",
+  //   "ice",
+  //   "grass",
+  // ];
   const [results, overlap, sharedWeakness] = getIdealTeam(query);
 
   if (overlap.length) {
     console.log("query:", query);
     console.log("Overlapping inputs:", overlap);
     console.log("Shared weakness:", sharedWeakness);
+  } else {
+    console.log("no overlapping weaknesses!");
+    console.log("query:", query);
   }
 
-  console.log(`results:`, results);
+  const flatResults = Array.from(new Set(results.flat()));
+  const uniqueInputs = Array.from(new Set(query.flat()));
+  const difference = <T>(a: T[], b: T[]) => a.filter((x) => !b.includes(x));
+  const additions = difference(flatResults, uniqueInputs);
+  const hasDifference = additions.length > 0;
+
+  if (hasDifference) {
+    console.log(`recommended team type additions:`, additions);
+  }
 }
 
 main();
